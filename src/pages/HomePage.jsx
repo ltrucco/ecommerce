ffed05e6ff3bcc90682d2d641b03b5bc65829110
ltrucco <56170216+ltrucco@users.noreply.github.com>
2021-01-24 19:06:40 -0300

    Base page structure

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './HomePageStyles.scss'
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="directoryMenu">
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">Hats</h1>
+                        <span className="subtitle">Show now</span>
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">Hats</h1>
+                        <span className="subtitle">Show now</span>
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">Hats</h1>
+                        <span className="subtitle">Show now</span>
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">Hats</h1>
+                        <span className="subtitle">Show now</span>
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">Hats</h1>
+                        <span className="subtitle">Show now</span>
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">Hats</h1>
+                        <span className="subtitle">Show now</span>
+                    </div>
+                </div>
+                <div className="menuItem">
+                    <div className="content">
+                        <h1 className="title">Hats</h1>
+                        <span className="subtitle">Show now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
